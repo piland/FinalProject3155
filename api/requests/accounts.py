b6418ff6008d6893b0f87732cf668a14b5bb775d
Jack Martin <jackamartin17@gmail.com>
2024-08-04 17:48:43 -0400

    updated account requests

--- api/requests/accounts.py
+++ api/requests/accounts.py
@@ -3,7 +3,7 @@ import requests
 base_url = "http://127.0.0.1:8000"
 
 
-def create(name, age, phone_number, email, role_id, password):
+def create(name, age, phone_number, email, role_id, password, payment_information_id):
     url = f"{base_url}/accounts/"
     data = {
         "name": name,
@@ -11,7 +11,8 @@ def create(name, age, phone_number, email, role_id, password):
         "phone_number": phone_number,
         "email": email,
         "role_id": role_id,
-        "password": password
+        "password": password,
+        "payment_information_id": payment_information_id
     }
     response = requests.post(url, json=data)
     if response.status_code == 200:
@@ -44,7 +45,8 @@ def read_one(account_id):
         return None
 
 
-def update(account_id, name=None, age=None, phone_number=None, email=None, role_id=None, password=None):
+def update(account_id, name=None, age=None, phone_number=None, email=None, role_id=None, password=None,
+           payment_information_id=None):
     url = f"{base_url}/accounts/{account_id}"
     new_data = {}
     if name is not None:
@@ -59,6 +61,8 @@ def update(account_id, name=None, age=None, phone_number=None, email=None, role_
         new_data["role_id"] = role_id
     if password is not None:
         new_data["password"] = password
+    if payment_information_id is not None:
+        new_data["payment_information_id"] = payment_information_id
     response = requests.put(url, json=new_data)
     if response == 200:
         print(f"Payment Info Updated: {response.json()}")
