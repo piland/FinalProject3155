3b1886fc25e63ea59df609e59f9522ff2405bbcd
Jack Martin <jackamartin17@gmail.com>
2024-08-05 15:53:48 -0400

    I dont know whatever man

--- api/models/orders.py
+++ api/models/orders.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DATETIME
 from sqlalchemy.orm import relationship, mapped_column
 from datetime import datetime
 from api.dependencies.database import Base
-from api.models.accounts import Account
+
 
 class Order(Base):
     __tablename__ = "orders"
@@ -16,8 +16,5 @@ class Order(Base):
 
     accounts = relationship('Account', back_populates="orders")
     order_details = relationship("OrderDetail", back_populates="order")
-
-    def __repr__(self):
-        return f"ID: {self.id}, NAME: {self.customer_name}, DATE: {self.order_date}, DESC: {self.description}, TYPE: {self.order_type}, ACCOUNT: {self.account_id}"
 
 
